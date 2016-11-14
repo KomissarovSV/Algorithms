3e3c5452ed213ce04b35e3cc3a7817b12ee22629
KomissarovSV <komissarov1371@gmail.com>
2016-11-14 18:58:16 +0300

    QuickSort

---
+++ Sorting/QuickSort.py
@@ -0,0 +1,30 @@
+def partition(data,start,end):
+    pivot = data[start]
+    i = start + 1
+    j = end
+    while True:
+        while data[i] < pivot and i != j:
+            i += 1
+        while data[j] > pivot:
+            j -= 1
+        if i >= j:
+            break
+        data[i],data[j] = data[j],data[i]
+    data[start],data[j] = data[j],data[start]
+    return j
+
+def sort(data,start = 0,end = None):
+    if end is None:
+        end = len(data) - 1
+    if start >= end:
+        return
+    pivot = partition(data,start,end)
+    sort(data,start,pivot - 1)
+    sort(data,pivot + 1,end)
+
+
+a = [1,5,8,38,34,-4,-7,-2,56]
+sort(a)
+print(a)
+
+
